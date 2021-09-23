baacf90dbd067e7fc6ad1caa2362483dcd84461f
Daniel <daniel.s.dash@gmail.com>
2021-09-23 09:47:59 -0400

    updated restaurant.py

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,3 +1,13 @@
 def draw_restaurant():
-    print("restaurant not found")
+    print("Welcome to Cafe Daince!")
+    print("|----------------------------|")
+    print("| [    ]              [    ] |")
+    print("| [====]              [====] |")
+    print("| [ || ]              [ || ] |")
+    print("|    || LE CAFE DAINCE ||    |")
+    print("|----------------------------|")
+    print("| [    ]   ________   [    ] |")
+    print("| [====]  |   __   |  [====] |")
+    print("| [ || ]  |  |  |  |  [ || ] |")
+    print("|_________|  |  |  |_________|")
     return
